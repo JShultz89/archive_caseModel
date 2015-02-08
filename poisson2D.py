4e9ce76438cf5657b227fdcd263d3f65d8f8700f
zaidedan <dan.zaide@gmail.com>
2015-02-08 16:40:23 -0500

    code somewhat verified

--- poisson2D.py
+++ poisson2D.py
@@ -1,9 +1,11 @@
 """
 Solve u_xx + u_yy = f(x,y) on a uniform grid
+and v_xx + v_yy = f(x,y) on a uniform grid
 
 with known solution
 
-u = exp(xy) , f = (x^2+y^2)*exp(xy)
+u = exp(xy) 	 				f['u'] = (x^2+y^2)*exp(xy)
+v = exp((x^2+y^2))  f['v'] = (4*(x^2+y^2+1))*exp((x^2+y^2)) 
 
 python poisson2D n
 for the number of cells in each dimension
@@ -19,18 +21,19 @@ import src.source as s
 def poisson2D(N):
 	n = N+2 # add two for the boundaries
 	""" 
-	lets define a uniform square mesh on [0, 1] x [0, 1]
+	lets define a uniform square mesh on [-1, 1] x [1, 1]
 	and create boundary blocks as we go,
 	initializing based on the exact solution, and naming the block by its coordinates
 	"""
-	d = 1/float(n-2) # spacing, delta X
-	B = [b.Block('('+str(i*d-d/2)+','+str(j*d-d/2)+')',None,{'u':math.exp((i*d-d/2)*(j*d-d/2))}) for i in range(0,n) for j in range(0,n)]
+	d = 2./float(n-2) # spacing, delta X
+	B = [b.Block('('+str(i*d-d/2-1)+','+str(j*d-d/2-1)+')',None,{'u':math.exp((i*d-d/2-1)*(j*d-d/2-1)),
+		'v':math.exp((i*d-d/2-1)**2+(j*d-d/2-1)**2)}) for i in range(0,n) for j in range(0,n)]
 
 	# interior sources
 	# use the name to get the source values
 	for block in B:
 		(x,y) = eval(block.name)
-		block.addSource(s.Source('const',u = -(x*x+y*y)*math.exp(x*y)))
+		block.addSource(s.Source('const',u = -(x*x+y*y)*math.exp(x*y),v = -4.0*(x*x+y*y+1.0)*math.exp(x*x+y*y)))
 	# Flux geometry	
 	G = {'type':'diff','d':d*d,'m':[]}
 	intB = []
@@ -42,15 +45,23 @@ def poisson2D(N):
 			B[i*n+j].addFlux(f.Flux(B[k[1]],B[i*n+j],'difference',G))
 			B[i*n+j].addFlux(f.Flux(B[i*n+j],B[k[2]],'difference',G))
 			B[i*n+j].addFlux(f.Flux(B[i*n+j],B[k[3]],'difference',G))
+			B[i*n+j].state['u'] = 0.
+			B[i*n+j].state['v'] = 0.
+
 			intB.append(B[i*n+j])
 	
 	# solve the problem on the interior blocks
 	P = p.Problem(intB)
 	P.solve()
-
-	return math.sqrt(sum([(math.exp(eval(block.name)[0]*eval(block.name)[1])-block.state['u'])**2 for block in intB])/(n-2)/(n-2))
+	Eu = math.sqrt(sum([(math.exp(eval(block.name)[0]*eval(block.name)[1])-block.state['u'])**2 for block in intB])/(n-2)/(n-2))
+	Ev = math.sqrt(sum([(math.exp(eval(block.name)[0]**2+eval(block.name)[1]**2)-block.state['v'])**2 for block in intB])/(n-2)/(n-2))
+	return (Eu,Ev)
 
 if __name__ == "__main__":
-	""
-	Error = [poisson2D(5),poisson2D(10),poisson2D(20)]
-	Rate = [-math.log(Error[0]-Error[1])/math.log(75),-math.log(Error[1]-Error[2])/math.log(300)]
+	n = int(sys.argv[1])
+	# print poisson2D(2)
+	Error = [poisson2D(n),poisson2D(n*2)]
+	print Error
+	Rate = [(math.log(Error[1][0])-math.log(Error[0][0]))/(math.log(2./(2*n))-math.log(2./(n))),
+	(math.log(Error[1][1])-math.log(Error[0][1]))/(math.log(2./(2*n))-math.log(2./(n)))]
+	print Rate
