26dbe63fd0ca36bf68c047e81702e1e6cc2b8deb
Justin Shultz <jss5237@gmail.com>
2015-02-18 21:34:10 -0500

    Revert "Fixed bug in num modules"
    This reverts commit a2438d6dd5dfcf8bbdf6bdadafc2a99bab80476f.

--- ICSolar.py
+++ ICSolar.py
@@ -52,7 +52,6 @@ import src.source as s
 
 """ Optional Modules """
 import csv
-import sys
 
 def solve(heatGen,waterTemp,n):
 	""" Boundary flux blocks """
@@ -88,6 +87,8 @@ def solve(heatGen,waterTemp,n):
 
 	""" Block Initialization """
 
+	# Number of modules
+	n = 2
 	# Initial lists of blocks
 	water = []
 	air = []
@@ -103,8 +104,8 @@ def solve(heatGen,waterTemp,n):
 
 		if(i % 2 == 1): # odd regions are "tube" regions
 			# Every block is named for its material in this case
-			water.append(b.Block('waterTube' + str((i+1)/2),'water',T = 15))
-			air.append(b.Block('airTube' + str((i+1)/2),'air',T = 22))
+			water.append(b.Block('waterTube' + str(i),'water',T = 15))
+			air.append(b.Block('airTube' + str(i),'air',T = 22))
 			# Water tube has one flux for heat conduction
 			if( i == 1 ): 
 				water[i].addFlux(f.Flux(air[i],'heatCondSimple',{'type':'wa','m':[],'L':0.15}))
@@ -119,8 +120,8 @@ def solve(heatGen,waterTemp,n):
 				air[i].addFlux(f.Flux(aExt,'heatCondSimple',{'type':'ext','m':[],'L':0.3}))
 		else: # These are "module" region
 			# Every block is named for its material in this case
-			water.append(b.Block('waterModule' + str(i/2),'water',T = 15))
-			air.append(b.Block('airModule' + str(i/2),'air',T = 22))
+			water.append(b.Block('waterModule' + str(i),'water',T = 15))
+			air.append(b.Block('airModule' + str(i),'air',T = 22))
 			water[i].addSource(Sw)
 			air[i].addSource(Sa)
 
@@ -147,7 +148,7 @@ def solve(heatGen,waterTemp,n):
 	return water[-1].state['T']
 
 if __name__ == "__main__":
-	if len(sys.argv) < 4:
+	if(1):
 		csvfile = open('nov25.csv','rU')
 		csvwrite = open('simulation.csv','w')
 		cr = csv.DictReader(csvfile)
@@ -161,5 +162,3 @@ if __name__ == "__main__":
 				'exp_outlet':row['exp_outlet'],'sim_outlet':round(Tf,8),'exp_heatgen':row['exp_heatgen']})
 		csvfile.close()
 		csvwrite.close()
-	else:
-		print solve(float(sys.argv[1]),float(sys.argv[2]),int(sys.argv[3]))
