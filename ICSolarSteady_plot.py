aa58d03736c2db18a47d5ce76736e445f980e2cb
zaidedan <dan.zaide@gmail.com>
2015-03-18 08:15:12 -0400

    added images

--- ICSolarSteady_plot.py
+++ ICSolarSteady_plot.py
@@ -6,19 +6,19 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 from subprocess import call
 from os import chdir
-
+import numpy as np
+import math
 if __name__ == "__main__":
 	basenames = ['Jan28','Jan31','Feb6','Feb11','Feb27','Feb28','Mar06','Mar09']
 	hlist = [0.01,0.1,0.5,0.75,1.0,1.25,1.5,1.75,2.0,5.0,10.0,100.0]
 
-	for name in basenames:
-		for h in hlist:
-			print name, h
-			call('python '+'ICSolar.py data/ICSolar/'+name+'.csv '+ str(h)+'>/dev/null',shell=True)
+	# for name in basenames:
+	# 	for h in hlist:
+	# 		print name, h
+	# 		call('python '+'ICSolar.py data/ICSolar/'+name+'.csv '+ str(h)+'>/dev/null',shell=True)
 
 	os.chdir('data/ICSolar')
 	for name in basenames:
-
 		for h in hlist:
 			h = float(h)
 			expt = open(name+'.csv','rU')
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 				csvfiles[filename].close()
 			# need to align the times
 			numPoints = len(data['exp']['Timestamp'])
-			t = range(0,numPoints)*10.0
+			t = np.arange(0,numPoints)*10.0
 	
 			plt.plot(t,data['exp']['exp_flowrate'],linewidth=2.0)
 			plt.xlabel('Time (seconds)')
@@ -41,7 +41,7 @@ if __name__ == "__main__":
 			fig = plt.gcf()
 			fig.set_size_inches(4,4)
 			axes = plt.gca()
-			plt.savefig('../../images/ICSolar/' + name+'_'+str(int(round(hwa*100)))+'_flowrate.png')		
+			plt.savefig('../../images/ICSolar/' + name+'_'+str(int(round(h*100)))+'_flowrate.png')		
 			plt.close()
 			for j in range(6,0,-1):
 				plt.plot(t,data['exp']['heatgen_m'+str(j)],linewidth=2.0,label=str(j))
@@ -49,7 +49,7 @@ if __name__ == "__main__":
 			plt.ylabel('Q_i')
 			fig = plt.gcf()
 			fig.set_size_inches(4,4)
-			plt.savefig('../../images/ICSolar/' + name+'_'+str(int(round(hwa*100)))+'_Q.png')		
+			plt.savefig('../../images/ICSolar/' + name+'_'+str(int(round(h*100)))+'_Q.png')		
 			plt.close()
 
 			for j in range(6,0,-1):
@@ -59,7 +59,7 @@ if __name__ == "__main__":
 
 				L2 = sum([(data['exp']['m'+str(j)+'_in'][i]-data['st']['m'+str(j)+'_in'][i])*(data['exp']['m'+str(j)+'_in'][i]-data['st']['m'+str(j)+'_in'][i])
 				 for i in range(0,numPoints)])
-				L2 = sqrt(L2/numPoints)
+				L2 = math.sqrt(L2/numPoints)
 				Linf = max([abs(data['exp']['m'+str(j)+'_in'][i]-data['st']['m'+str(j)+'_in'][i]) for i in range(0,numPoints)])
 
 				plt.legend(loc=0)
@@ -70,7 +70,7 @@ if __name__ == "__main__":
 				fig.set_size_inches(4,4)
 				axes = plt.gca()
 				axes.set_ylim([20,100])
-				plt.savefig('../../images/ICSolar/' + name+'_m'+str(j)+'_out_steady_'+str(int(round(hwa*100)))+'.png')		
+				plt.savefig('../../images/ICSolar/' + name+'_m'+str(j)+'_out_steady_'+str(int(round(h*100)))+'.png')		
 				plt.close()
 
 				plt.plot(t,data['exp']['m'+str(j)+'_out'],linewidth=2.0,label='Expt')
@@ -78,7 +78,7 @@ if __name__ == "__main__":
 				plt.plot(t,data['exp']['exp_inlet'],linewidth=2.0,label='Inlet')
 				L2 = sum([(data['exp']['m'+str(j)+'_out'][i]-data['st']['m'+str(j)+'_out'][i])*(data['exp']['m'+str(j)+'_out'][i]-data['st']['m'+str(j)+'_out'][i])
 				 for i in range(0,numPoints)])
-				L2 = sqrt(L2/numPoints)
+				L2 = math.sqrt(L2/numPoints)
 				Linf = max([abs(data['exp']['m'+str(j)+'_out'][i]-data['st']['m'+str(j)+'_out'][i]) for i in range(0,numPoints)])
 
 				plt.legend(loc=0)
@@ -89,12 +89,12 @@ if __name__ == "__main__":
 				fig.set_size_inches(4,4)
 				axes = plt.gca()
 				axes.set_ylim([20,100])
-				plt.savefig('../../images/ICSolar/' + name+'_m'+str(j)+'_out_steady_'+str(int(round(hwa*100)))+'.png')
+				plt.savefig('../../images/ICSolar/' + name+'_m'+str(j)+'_out_steady_'+str(int(round(h*100)))+'.png')
 				plt.close()
 
 				# lets do this manually
 			os.chdir('../../_posts/')
-			mdfilename = time.strftime("%Y-%m-%d")+'-Steady_'+name+str(int(round(hwa*100)))+'.md'
+			mdfilename = time.strftime("%Y-%m-%d")+'-Steady_'+name+'_'+str(int(round(h*100)))+'.md'
 			mdfile = open(mdfilename,'w')
 			header = ['---','layout: post','title: Comparison of data on '+name,
 				'---', '{{ page.title }}','-----------------', 'With h_{wa} = '+str(h*0.16*0.3)]
@@ -103,14 +103,14 @@ if __name__ == "__main__":
 				mdfile.write(item+'\n')
 			mdfile.write('\n')
 			mdfile.write('![Results]({{ site.baseurl }}/images/ICSolar/' + \
-					name+'_'+str(int(round(hwa*100)))+'_flowrate.png ')
+					name+'_'+str(int(round(h*100)))+'_flowrate.png ')
 			mdfile.write('![Results]({{ site.baseurl }}/images/ICSolar/' + \
-					name+'_'+str(int(round(hwa*100)))+'_Q.png\n\n')
+					name+'_'+str(int(round(h*100)))+'_Q.png\n\n')
 			for i in range(6,0,-1):
 				mdfile.write('![Results]({{ site.baseurl }}/images/ICSolar/' + \
-					name+'_m'+str(j)+'_in_steady_'+str(int(round(hwa*100)))+'.png ')
+					name+'_m'+str(j)+'_in_steady_'+str(int(round(h*100)))+'.png ')
 				mdfile.write('![Results]({{ site.baseurl }}/images/ICSolar/' + \
-					name+'_m'+str(j)+'_out_steady_'+str(int(round(hwa*100)))+'.png\n\n')
+					name+'_m'+str(j)+'_out_steady_'+str(int(round(h*100)))+'.png\n\n')
 
 			mdfile.close()
 			os.chdir('../data/ICSolar')
